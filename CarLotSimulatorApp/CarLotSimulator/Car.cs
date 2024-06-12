27c93f05d8587fe1e76170c363dd4d4474ca938e
Michele Mostoller <MMostoller@att.net>
2024-06-12 17:17:54 -0400

    Fixed Exercise

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLotSimulator
+{
+
+
+
+
+    public class Car
+    {
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+        public bool IsDriveable { get; set; }
+
+        public void MakeEngineNoise(string noise)
+        {
+            Console.WriteLine($"The {Year} {Make} {Model} makes the following engine noise: {noise}");
+        }
+
+        public void MakeHonkNoise(string noise)
+        {
+            Console.WriteLine($"The {Year} {Make} {Model} makes the following honk noise: {noise}");
+        }
+    }
+}
+
+
+
+
